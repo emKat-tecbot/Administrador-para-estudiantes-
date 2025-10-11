fa6f14c5df1f0a7094f8d472fe79674299ab5f13
EmKat-tecbot <161508294+emKat-tecbot@users.noreply.github.com>
2025-10-10 21:06:19 -0400

    Create calificacionMenu.py

---
+++ main/calculadora_de_callificaciones/calificacionMenu.py
@@ -0,0 +1,9 @@
+def menu ():
+  print(f'''
+  1. Caificaion por clase
+  2. ¿Qué nota necesito para pasar?
+  3. ¿Qué tanto he progresado con esta nota?
+  4. Plan de estudio
+  ''')
+  optCal = int(input("Que quuieres hacer? "))
+  return optCal
