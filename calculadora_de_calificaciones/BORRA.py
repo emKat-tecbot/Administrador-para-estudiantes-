099960676bc81c3c72b59abe8fa0b540e83ff76f
Emma Molina <EmKat-tecbot>
2025-10-16 10:42:13 -0400

    cambios a calucladora

--- calculadora_de_calificaciones/BORRA.py
+++ calculadora_de_calificaciones/BORRA.py
@@ -4,15 +4,15 @@ def calendario ( temas, horasLibres, dificultad, hoy, fechaExamen):
     temasRep = []
     calendario = [["Dia","Horas a estudiar","Temas a estudiar","Topico de enfoque"]]
     for i in range (len(temas)):
-        temasRep.extend(temas * dificultad[i])
-                        
+        temasRep.extend(temas * dificultad[i])                      
     temasDeHoy = []
     for a in range (len(temasRep)):
-        for b in range (min(3,len(temasRep))):
+        temasDeHoy.append([])
+        for b in range (0,min(3,len(temasRep))):
             tema = random.choice(temasRep) 
-            temasDeHoy[b].append(tema)
-        for i in range (1, dias + 1):
+            temasDeHoy[a].append(tema)
+        for c in range (1, dias + 1):
             for j in range (len(horasLibres)):
                 calendario.append([j + 1,horasLibres[j],temasDeHoy[j]])
-    print(calendario)
+    print(temasDeHoy)
 calendario(["Algebra","Trigonometria","Calculo"],[3,2,4],[3,2,5],1,7)
